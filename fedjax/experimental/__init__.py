dabe4500a7af364594148abe45496555a285b0b7
Jae Ro <jaero@google.com>
2021-04-28 09:50:42 -0700

    Client samplers for fixed uniform client samples for training
    PiperOrigin-RevId: 370924917

--- fedjax/experimental/__init__.py
+++ fedjax/experimental/__init__.py
@@ -20,6 +20,7 @@ backward compatibility. Certain modules may not even work.
 from fedjax.experimental import aggregators
 from fedjax.experimental import algorithms
 from fedjax.experimental import client_datasets
+from fedjax.experimental import client_samplers
 from fedjax.experimental import datasets
 from fedjax.experimental import federated_algorithm
 from fedjax.experimental import federated_data
