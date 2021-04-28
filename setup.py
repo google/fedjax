7c58d74785c340d39b2570c07b0caaa611227269
Ke Wu <wuke@google.com>
2021-04-28 15:12:00 -0700

    Remove an extra newline.
    ```
        ▄▄▄▄      ▄▄▄▄▄▄▄▄▄    ▄▄▄▄▄▄▄▄▄
      ▄█░░░░▌    ▐░░░░░░░░░▌  ▐░░░░░░░░░▌
     ▐░░▌▐░░▌   ▐░█░█▀▀▀▀▀█░▌▐░█░█▀▀▀▀▀█░▌
      ▀▀ ▐░░▌   ▐░▌▐░▌    ▐░▌▐░▌▐░▌    ▐░▌
         ▐░░▌   ▐░▌ ▐░▌   ▐░▌▐░▌ ▐░▌   ▐░▌
         ▐░░▌   ▐░▌  ▐░▌  ▐░▌▐░▌  ▐░▌  ▐░▌
         ▐░░▌   ▐░▌   ▐░▌ ▐░▌▐░▌   ▐░▌ ▐░▌
         ▐░░▌   ▐░▌    ▐░▌▐░▌▐░▌    ▐░▌▐░▌
     ▄▄▄▄█░░█▄▄▄▐░█▄▄▄▄▄█░█░▌▐░█▄▄▄▄▄█░█░▌
    ▐░░░░░░░░░░░▌▐░░░░░░░░░▌  ▐░░░░░░░░░▌
     ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀    ▀▀▀▀▀▀▀▀▀
    ```
    
    Co-authored-by: jaehunro <jaero@google.com>
    Co-authored-by: kho <wuke@google.com>
    Co-authored-by: stheertha <theertha@google.com>
    PiperOrigin-RevId: 370995354

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Install fedjax."""
 
 from setuptools import find_namespace_packages
