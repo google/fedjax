Correcting basic FedAvg implementation to use size of client dataset as weight in average rather than number of examples seen during training
PiperOrigin-RevId: 370918948

fedjax/experimental/examples/fed_avg.py
@@ -36,10 +36,10 @@ from fedjax.experimental import client_datasets
 from fedjax.experimental import federated_algorithm
 from fedjax.experimental import federated_data
 from fedjax.experimental import optimizers
+from fedjax.experimental import tree_util as exp_tree_util
 from fedjax.experimental.typing import BatchExample
 
 import jax
-import jax.numpy as jnp
 
 Grads = Params
 
@@ -74,37 +74,33 @@ def federated_averaging(
                               client_datasets.ClientDataset, PRNGKey]]
   ) -> Tuple[ServerState, Mapping[federated_data.ClientId, Any]]:
     client_diagnostics = {}
-    client_outputs = []
+    client_deltas_weights = []
     for client_id, client_dataset, client_rng in clients:
-      delta_params, num_examples, step_results = client_update(
-          server_state.params, client_dataset, client_rng)
-      client_outputs.append((delta_params, num_examples))
-      client_diagnostics[client_id] = step_results
-    server_state = server_update(server_state, client_outputs)
+      delta_params, num_examples = client_update(server_state.params,
+                                                 client_dataset, client_rng)
+      client_deltas_weights.append((delta_params, num_examples))
+      # We record the l2 norm of client updates as an example, but it is not
+      # required for the algorithm.
+      client_diagnostics[client_id] = {
+          'delta_l2_norm': exp_tree_util.tree_l2_norm(delta_params)
+      }
+    server_state = server_update(server_state, client_deltas_weights)
     return server_state, client_diagnostics
 
   def client_update(server_params, client_dataset, client_rng):
     params = server_params
     opt_state = client_optimizer.init(params)
-    num_examples = 0.
-    step_results = []
     for batch in client_dataset.shuffle_repeat_batch(client_dataset_hparams):
       client_rng, use_rng = jax.random.split(client_rng)
       grads = grad_fn(params, batch, use_rng)
       opt_state, params = client_optimizer.apply(grads, opt_state, params)
-      num_examples += len(next(iter(batch.values())))
-      # We record the l2 norm of gradients as an example, but it is not required
-      # for the algorithm.
-      step_results.append({
-          'grad_l2_norm':
-              sum(jnp.vdot(x, x) for x in jax.tree_util.tree_leaves(grads))
-      })
     delta_params = jax.tree_util.tree_multimap(lambda a, b: a - b,
                                                server_params, params)
-    return delta_params, num_examples, step_results
+    num_examples = len(client_dataset)
+    return delta_params, num_examples
 
-  def server_update(server_state, client_outputs):
-    delta_params = tree_util.tree_mean(client_outputs)
+  def server_update(server_state, client_deltas_weights):
+    delta_params = tree_util.tree_mean(client_deltas_weights)
     opt_state, params = server_optimizer.apply(delta_params,
                                                server_state.opt_state,
                                                server_state.params)

fedjax/experimental/examples/fed_avg_test.py
@@ -11,31 +11,35 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for fedjax.experimental.examples.fed_avg_basic."""
+"""Tests for fedjax.experimental.examples.fed_avg."""
 from absl.testing import absltest
 
 from fedjax.experimental import client_datasets
 from fedjax.experimental import optimizers
-from fedjax.experimental.examples import fed_avg_basic
+from fedjax.experimental.examples import fed_avg
 
 import jax
 import jax.numpy as jnp
 import numpy.testing as npt
 
 
-class FedAvgBasicTest(absltest.TestCase):
+def grad_fn(params, batch, rng):
+  del rng
+  return jax.tree_util.tree_map(lambda l: l / jnp.sum(batch['x']), params)
+
+
+class FedAvgTest(absltest.TestCase):
 
   def test_federated_averaging(self):
-    grad_fn = lambda p, b, r: jax.tree_util.tree_map(lambda l: l * 0.5, p)
     client_optimizer = optimizers.sgd(learning_rate=1.0)
     server_optimizer = optimizers.sgd(learning_rate=1.0)
     client_dataset_hparams = client_datasets.ShuffleRepeatBatchHParams(
-        batch_size=2, num_epochs=1, seed=0)
-    fed_avg = fed_avg_basic.federated_averaging(grad_fn, client_optimizer,
-                                                server_optimizer,
-                                                client_dataset_hparams)
+        batch_size=2, num_epochs=2, seed=0)
+    algorithm = fed_avg.federated_averaging(grad_fn, client_optimizer,
+                                            server_optimizer,
+                                            client_dataset_hparams)
     with self.subTest('init'):
-      state = fed_avg.init({'w': jnp.array([0., 2., 4.])})
+      state = algorithm.init({'w': jnp.array([0., 2., 4.])})
       npt.assert_array_equal(state.params['w'], [0., 2., 4.])
       self.assertLen(state.opt_state, 2)
     with self.subTest('apply'):
@@ -47,18 +51,12 @@ class FedAvgBasicTest(absltest.TestCase):
            client_datasets.ClientDataset({'x': jnp.array([8., 10.])}),
            jax.random.PRNGKey(1)),
       ]
-      state, client_diagnostics = fed_avg.apply(state, clients)
-      npt.assert_array_almost_equal(state.params['w'], [0., 0.666667, 1.333333])
-      npt.assert_equal(
-          client_diagnostics, {
-              b'cid0': [
-                  {'grad_l2_norm': jnp.array(5.)},
-                  {'grad_l2_norm': jnp.array(1.25)}
-              ],
-              b'cid1': [
-                  {'grad_l2_norm': jnp.array(5.)}
-              ]
-          })
+      state, client_diagnostics = algorithm.apply(state, clients)
+      npt.assert_allclose(state.params['w'], [0., 1.4425802, 2.8851604])
+      npt.assert_allclose(client_diagnostics[b'cid0']['delta_l2_norm'],
+                          1.7553135)
+      npt.assert_allclose(client_diagnostics[b'cid1']['delta_l2_norm'],
+                          0.48310122)
 
 
 if __name__ == '__main__':

fedjax/experimental/tree_util.py
@@ -18,8 +18,18 @@ container-like Python objects.
 For more details, see https://jax.readthedocs.io/en/latest/pytrees.html.
 """
 
+from fedjax.experimental.typing import PyTree
+
 import jax
+import jax.numpy as jnp
 
 
-def tree_size(pytree) -> int:
+@jax.jit
+def tree_size(pytree: PyTree) -> int:
   return sum(l.size for l in jax.tree_util.tree_leaves(pytree))
+
+
+@jax.jit
+def tree_l2_norm(pytree: PyTree) -> float:
+  return jnp.sqrt(
+      sum(jnp.vdot(x, x) for x in jax.tree_util.tree_leaves(pytree)))