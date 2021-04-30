d112d7659398df4844842b52297bc1812c089142
Ananda Theertha Suresh <theertha@google.com>
2021-04-30 07:36:07 -0700

    Adding models to experimental init.py.
    PiperOrigin-RevId: 371330396

--- fedjax/experimental/__init__.py
+++ fedjax/experimental/__init__.py
@@ -27,6 +27,7 @@ from fedjax.experimental import federated_data
 from fedjax.experimental import for_each_client
 from fedjax.experimental import metrics
 from fedjax.experimental import model
+from fedjax.experimental import models
 from fedjax.experimental import optimizers
 from fedjax.experimental import regularizers
 from fedjax.experimental import serialization
