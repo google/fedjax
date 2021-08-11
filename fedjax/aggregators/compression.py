Fixing uniform_stochastc_quantize and adding more tests. - The previous code was numerically unstable when all the elements of a vector had a same value. - The stochastic uniform compression needs to provide an unbiased estimator of the true mean. However, the previous code was not. Fixed it and added tests to verify the same.
PiperOrigin-RevId: 390184465

fedjax/aggregators/compression.py
@@ -49,7 +49,7 @@ def binary_stochastic_quantize(v, rng):
   """
   v_min = jnp.amin(v)
   v_max = jnp.amax(v)
-  v = (v - v_min) / (v_max - v_min + 1e-15)
+  v = jnp.nan_to_num((v - v_min) / (v_max - v_min))
   rand = jax.random.uniform(key=rng, shape=v.shape)
   return jnp.where(rand > v, v_min, v_max)
 
@@ -68,13 +68,14 @@ def uniform_stochastic_quantize(v, num_levels, rng):
   # Rescale the vector to be between zero to one.
   v_min = jnp.amin(v)
   v_max = jnp.amax(v)
-  v = (v - v_min) / (v_max - v_min + 1e-15)
+  v = jnp.nan_to_num((v - v_min) / (v_max - v_min))
   # Compute the upper and lower boundary of each value.
   v_ceil = jnp.ceil(v * (num_levels - 1)) / (num_levels - 1)
   v_floor = jnp.floor(v * (num_levels - 1)) / (num_levels - 1)
   # uniformly quantize between v_ceil and v_floor.
   rand = jax.random.uniform(key=rng, shape=v.shape)
-  quantized = jnp.where(rand > v, v_floor, v_ceil)
+  threshold = jnp.nan_to_num((v - v_floor) / (v_ceil - v_floor))
+  quantized = jnp.where(rand > threshold, v_floor, v_ceil)
   # Rescale the values and return it.
   return v_min + quantized * (v_max - v_min)
 

fedjax/aggregators/compression_test.py
@@ -26,19 +26,56 @@ class CompressionTest(absltest.TestCase):
     params = {'w': jnp.array([1., 2., 3.]), 'b': jnp.array([1.])}
     self.assertEqual(compression.num_leaves(params), 2)
 
-  def test_binary_stochastic_quantize(self):
+  def test_binary_stochastic_quantize_identity(self):
     # If the vector has only two distinct values, it should not change.
     v = jnp.array([0., 2., 2.])
     rng = jax.random.PRNGKey(42)
     compressed_v = compression.binary_stochastic_quantize(v, rng)
     npt.assert_array_equal(compressed_v, v)
 
-  def test_uniform_stochastic_quantize(self):
-    # If the vector has only three distinct values, it should not change.
+  def test_binary_stochastic_quantize_unbiasedness(self):
+    # If the vector has only two distinct values, it should not change.
     v = jnp.array([0., 1., 2.])
+    rng = jax.random.PRNGKey(42)
+    compressed_sum = jnp.zeros_like(v)
+    for _ in range(500):
+      rng, use_rng = jax.random.split(rng)
+      compressed_sum += compression.binary_stochastic_quantize(v, use_rng)
+    npt.assert_array_almost_equal(compressed_sum / 500, v, decimal=2)
+
+  def test_uniform_stochastic_quantize_identity(self):
+    # If the vector has only two distinct values, it should not change.
+    v = jnp.array([0., 2., 2., 4.])
     rng = jax.random.PRNGKey(42)
     compressed_v = compression.uniform_stochastic_quantize(v, 3, rng)
     npt.assert_array_equal(compressed_v, v)
+
+  def test_uniform_stochastic_quantize_all_equal(self):
+    # If the vector has only two distinct values, it should not change.
+    v = jnp.array([4., 4., 4., 4.])
+    rng = jax.random.PRNGKey(42)
+    compressed_v = compression.uniform_stochastic_quantize(v, 4, rng)
+    npt.assert_array_equal(compressed_v, v)
+
+  def test_uniform_stochastic_quantize_unbiasedness_one_dim(self):
+    # If the vector has only three distinct values, it should not change.
+    v = jnp.array([0., 1., 100.])
+    rng = jax.random.PRNGKey(42)
+    compressed_sum = jnp.zeros_like(v)
+    for _ in range(500):
+      rng, use_rng = jax.random.split(rng)
+      compressed_sum += compression.uniform_stochastic_quantize(v, 125, use_rng)
+    npt.assert_array_almost_equal(compressed_sum / 500, v, decimal=2)
+
+  def test_uniform_stochastic_quantize_unbiasedness_two_dim(self):
+    # If the vector has only three distinct values, it should not change.
+    v = jnp.array([[0., 1., 100.], [0.3, 2.3, 45.]])
+    rng = jax.random.PRNGKey(42)
+    compressed_sum = jnp.zeros_like(v)
+    for _ in range(500):
+      rng, use_rng = jax.random.split(rng)
+      compressed_sum += compression.uniform_stochastic_quantize(v, 125, use_rng)
+    npt.assert_array_almost_equal(compressed_sum / 500, v, decimal=1)
 
   def test_uniform_stochastic_quantizer(self):
     delta_params_and_weights = [('a', {